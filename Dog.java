625f70238cf4cb7973f837be6dcf64a1a59f448d
ttdangg <ttdang1445@gmail.com>
2025-01-08 10:21:04 -0800

    dog class

--- Dog.java
+++ Dog.java
@@ -5,12 +5,41 @@ public class Dog
   private static int numDogs = 0;
 
   // Non-default Dog constructor
+  public Dog(String name)
+  {
+    this.name = name;
+    numDogs++; 
+  }
 
   // Default Dog constructor
 
   // Setters - instance variables only
+  public void setName(String name)
+  {
+    name = iName
+  }
+  public void setID(int ID)
+  {
+    ID = iID
+  }
 
   // Getters - instance variables only
+  public String getName()
+  {
+    return name;
+  }
+  public int getID()
+  {
+    return ID;
+  }
 
   // method getNumDogs
+  public static int getNumDogs()
+  {
+    return numDogs;
+  }
+
+
+
+
 }
